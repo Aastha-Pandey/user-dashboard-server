8a5127141b58ed08c1305285e5c0954f71e5e52d
Aastha Pandey <aasthap7@gmail.com>
2022-03-13 17:38:19 +0530

    add user

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 import express from 'express';
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello m server!');
+
+const users = [
+  {
+    name: 'John',
+    username: 'john1',
+    address: 'New york',
+    phoneNumber: '2345671590',
+    email: 'john@gmail.com',
+    dateOfBirth: '7 jan 1980',
+  },
+  {
+    name: 'Harry',
+    username: 'harry1',
+    address: 'India',
+    phoneNumber: '6755671590',
+    email: 'harry@gmail.com',
+    dateOfBirth: '10 march 1989',
+  },
+  {
+    name: 'Clay',
+    username: 'clay1',
+    address: 'Paris',
+    phoneNumber: '3985791590',
+    email: 'clay@gmail.com',
+    dateOfBirth: '16 july 1995',
+  },
+];
+
+app.get('/user', (req, res) => {
+  res.json(users);
 });
 
 app.listen(8000, () => {
